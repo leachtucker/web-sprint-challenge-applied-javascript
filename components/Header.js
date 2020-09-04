5cc7d7adf5e62c0602ae508dbdba2f45923356da
Tucker Leach <leachtucker@gmail.com>
2020-09-04 08:30:56 -0700

    Header component finished

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,24 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // Set up parent element
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    // Set up child elements
+    const date = document.createElement('span');
+    date.classList.add('date');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = "Lambda Times";
+
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+
+    // Append elements
+    header.append(date, h1, temp);
+
+    // Return the parent element after all changes have been made
+    return header;
+}
