074e8052117494d1cd8d723ee9167b4348e99d95
Tucker Leach <leachtucker@gmail.com>
2020-09-04 09:54:23 -0700

    Articles added to DOM. MVP complete

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,76 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+// const { default: Axios } = require("axios");
+
+// IMPORT
+import axios from 'axios';
+
+// Find container to the cards
+const cardContainer = document.querySelector('div.cards-container');
+
+// Axios GET
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => {
+        const bootstrap = response.data["articles"].bootstrap;
+        const javascript = response.data["articles"].javascript;
+        const jquery = response.data["articles"].jquery;
+        const node = response.data["articles"].node;
+        const technology = response.data["articles"].technology;
+
+        // Creating a new array that contains the articles for all topics
+        const allArticles = bootstrap.concat(javascript, jquery, node, technology);
+
+        // Appending the cards to the DOM with article data from the allArticles array
+        appendCards(allArticles);
+    })
+    .catch(err => {
+        console.log(`There has been an error requesting the articles: ${err}`);
+    });
+
+// This function creates cards using the cardMaker component and then appends them to the DOM with data from an array of articles objects.
+function appendCards(articles) {
+    articles.forEach(article => {
+        const newCard = cardMaker(article);
+        cardContainer.append(newCard);
+    });
+}
+
+// Component
+function cardMaker(article) {
+    // Set up element
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Set up child elements
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = article["headline"];
+
+    const authorContainer = document.createElement('div');
+    authorContainer.classList.add('author');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    authorContainer.append(imgContainer);
+
+    const authorImg = document.createElement('img');
+    authorImg.setAttribute('src', article["authorPhoto"]);
+    imgContainer.append(authorImg);
+
+    const authorName = document.createElement('span');
+    authorName.textContent = article["authorName"];
+    authorContainer.append(authorName);
+
+    // Append child elements
+    card.append(headline, authorContainer);
+
+    // Event Listeners
+    card.addEventListener('click', function() {
+        console.log(card.querySelector('div.headline').textContent);
+    });
+
+    // Return created div.card element
+    return card;
+}
